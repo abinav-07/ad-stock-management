e8adb90086943b009d722980cbb0f78c27ad31f3
Satkar Kadel <leosatkar01@gmail.com>
2021-04-27 20:28:30 +0545

    Model Updates
    Validation and Attribute setup

--- Models/SalesDetail.cs
+++ Models/SalesDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,21 @@ namespace GroupCourseWork.Models
 {
     public class SalesDetail
     {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("SalId")]
+        [Display(Name = "Sales")]
+        public int SalesId { get; set; }
+        [ForeignKey("ProId")]
+        [Display(Name = "Product")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Quantity is Required")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+        [Display(Name = "Price ")]
+        public int Price { get; set; }
+        public virtual Sales SalId { get; set; }
+        public virtual Product ProId { get; set; }
     }
 }
