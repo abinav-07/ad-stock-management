181ed5dfaaa3475a55b1de475c73087f777549ef
Satkar Kadel <leosatkar01@gmail.com>
2021-04-28 14:13:55 +0545

    Model Update
    attribute in purchase detail

---
+++ Data/Migrations/20210428064616_Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupCourseWork.Data.Migrations
+{
+    public partial class Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "PurchaseDetail",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ManufactureDate",
+                table: "PurchaseDetail",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "PurchaseDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ManufactureDate",
+                table: "PurchaseDetail");
+        }
+    }
+}
