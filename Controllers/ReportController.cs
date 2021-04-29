1081051f7a8ead8367026f7ea154b212a35716f3
Satkar Kadel <leosatkar01@gmail.com>
2021-04-29 16:44:45 +0545

    Updates

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -55,6 +55,61 @@ namespace GroupCourseWork.Controllers
             return View(lstData);
 
         }
+
+        public IActionResult InactiveCustomerReport()
+        {
+            List<InactiveCustomerViewModel> lstData = new List<InactiveCustomerViewModel>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "SELECT FORMAT(SalesDate, 'MM-dd') AS Date,CustomerName from Customer c join sales s on c.id = s.CustomerId where DATEDIFF(day,SalesDate,GETDATE()) > 31 Order by day(SalesDate);";
+
+
+                _context.Database.OpenConnection();
+                using (var result = command.ExecuteReader())
+                {
+
+                    InactiveCustomerViewModel data;
+
+                    while (result.Read())
+                    {
+                        data = new InactiveCustomerViewModel();
+                        data.LastBoughtDate = result.GetDateTime(0);
+                        data.CustomerName = result.GetString(1);
+                        lstData.Add(data);
+                    }
+                }
+
+                return View(lstData);
+            }
+        }
+
+        public IActionResult InactiveProductReport()
+        {
+            List<InactiveProductViewModel> lstData = new List<InactiveProductViewModel>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "SELECT FORMAT(SalesDate, 'MM-dd') AS Date,ProductName ,Quantity from Product p join SalesDetail sd on p.id = sd.ProductId join Sales s on sd.SalesId = s.Id  where DATEDIFF(day,SalesDate,GETDATE()) > 31 Order by day(SalesDate);";
+
+
+                _context.Database.OpenConnection();
+                using (var result = command.ExecuteReader())
+                {
+
+                    InactiveProductViewModel data;
+
+                    while (result.Read())
+                    {
+                        data = new InactiveProductViewModel();
+                        data.LastBoughtDate = result.GetDateTime(0);
+                        data.ProductName = result.GetString(1);
+                        data.Quantity = result.GetInt32(2);
+                        lstData.Add(data);
+                    }
+                }
+
+                return View(lstData);
+            }
+        }
     }
    }
 
---
+++ ViewModels/InactiveProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupCourseWork.ViewModels
+{
+    public class InactiveProductViewModel
+    {
+        public DateTime LastBoughtDate { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
